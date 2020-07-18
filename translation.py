d23383a3d286676986a36afdfcdf2bd56a3be230
vaisakhc56 <64364212+vaisakhc56@users.noreply.github.com>
2020-07-18 13:35:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -27,7 +27,7 @@ This is a Telegram Rename Bot!
 1. Send me any Telegram File.
 2. Reply to that message to /rename new name.extension.
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/yYHrSnuYXpA">Diploy</a>
+<b>👉 Rename any file</b>
 
 --------
 
